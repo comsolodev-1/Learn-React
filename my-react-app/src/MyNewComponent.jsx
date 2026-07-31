336ab9e8cff5e8224af6a314681f6a388bfe8a16
soloDev <com.solodev@gmail.com>
2026-07-31 02:46:18 +0000

    added MyNewComponent for onChange events

--- my-react-app/src/MyNewComponent.jsx
+++ my-react-app/src/MyNewComponent.jsx
@@ -4,14 +4,22 @@ function MyNewComponent(){
     const [name, setName] = useState("random");
     const [comment, setComment] = useState("");
     const [choice, setChoice] = useState("");
-    const [radio, setRadio] = useState("");
+    const [radioVal, setRadio] = useState("");
 
     function handleName(event){
         setName(event.target.value);
     }
 
     function handleComment(event){
-        setComment(event.target.value);
+        setComment(event.target.value)
+    }
+
+    function handleChoice(event){
+        setChoice(event.target.value);
+    }
+
+    function handleRadio(event){
+        setRadio(event.target.value);
     }
 
     return(
@@ -19,8 +27,26 @@ function MyNewComponent(){
             <input value={name} onChange={handleName}/>
             <p>Name: {name}</p>
 
-            <textarea value={comment} onChange={handleComment}></textarea>
+            <textarea value={comment} onChange={handleComment} placeholder='write a comment'></textarea>
             <p>Comment: {comment}</p>
+
+            <select value={choice} onChange={handleChoice}>
+                <option value="">Select an option</option>
+                <option value="first choice">first choice</option>
+                <option value="second choice">second choice</option>
+                <option value="third choice">third choice</option>
+            </select>
+            <p>Choice: {choice}</p>
+
+            <label>
+                <input type="radio" value={"radio1"} checked={radioVal === "radio1"} onChange={handleRadio}/>
+                radio1
+            </label>
+            <label>
+                <input type="radio" value={"radio2"} checked={radioVal === "radio2"} onChange={handleRadio}/>
+                radio2
+            </label>
+            <p>Radio value: {radioVal}</p>
         </div>
     );
 }
